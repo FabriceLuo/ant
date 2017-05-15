937424769c5a06eab10d9744d83bfb4197861286
mike <luo_minghao@163.com>
2017-05-15 23:07:12 +0800

    add AntVersion,AntSvn and AntGit

---
+++ AntCommon.cpp
@@ -0,0 +1,6 @@
+#include "AntCommon.h"
+
+AntCommon::AntCommon()
+{
+
+}
---
+++ AntCommon.h
@@ -0,0 +1,11 @@
+#ifndef ANTCOMMON_H
+#define ANTCOMMON_H
+
+
+class AntCommon
+{
+public:
+    AntCommon();
+};
+
+#endif // ANTCOMMON_H
